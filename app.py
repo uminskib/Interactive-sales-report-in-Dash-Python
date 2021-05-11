e26d8e1a3e018d27948c7f17663f74fdab723473
uminskib <53011316+uminskib@users.noreply.github.com>
2021-05-12 00:01:47 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+
+import webbrowser
+import dash
+import dash_auth
+from log_in import user_password
+#inicjalizacja aplikacji dash
+app = dash.Dash(
+    __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
+)
+#Logowanie do strony
+auth = dash_auth.BasicAuth(
+    app,
+    user_password
+)
+app.title ='Raport' 
+server = app.server
+#instrukcja pozwalająca na używanie odwołań w kilku plikach
+app.config.suppress_callback_exceptions=True
+def open_browser():
+	webbrowser.open_new("http://localhost:{}".format('8050'))
